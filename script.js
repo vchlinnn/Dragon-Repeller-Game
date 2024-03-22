eab3b104d2179bfaed5dfbde4a2b77b32f33c024
vchlinnn <chuclinh190305@gmail.com>
2024-03-22 16:08:33 -0500

    get monster attack value

--- script.js
+++ script.js
@@ -186,8 +186,12 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks."
         text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
         //Health = health - monsters[fighting].level
-        health -= monsters[fighting].level;
-        monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+        health -= getMonsterAttackValue(monsters[fighting].level);
+        if (isMonsterHit()) {
+        monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1
+        }  else {
+          text.innerText += " You miss."
+        };
       healthText.innerText = health;
       monsterHealthText.innerText = monsterHealth;
          if (health <= 0) {
@@ -197,7 +201,21 @@ function attack() {
                           winGame()}
                     else {
                 defeatMonster()}
-      } 
+      };
+      if (Math.random() <= .1 && inventory.length !== 1) {
+        text.innerText += " Your " + inventory.pop() + " breaks.";
+        currentWeapon --
+      }
+}
+
+function getMonsterAttackValue(level){
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit);
+  return hit > 0 ? hit: 0
+}
+
+function isMonsterHit() { //Returns a boolean value 
+  return Math.random() > .2  || health < 20; //or
 }
 
 function dodge() {
@@ -224,6 +242,14 @@ function restart() {
       goTown()
 }
 
+function easterEgg() {
+  update(locations[7])
+}
+
+function pick(guess) {
+}
+
 function winGame() {
       update(locations[6])
 }
+
